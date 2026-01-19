e1bfd5388335417caf8de24c6d1d48f2b4a84d9a
manojunicorn <your-github-manojaug07@gmail.com>
2026-01-19 22:19:09 +0530

    Day:3 JavaScript practice

---
+++ functions.js
@@ -0,0 +1,11 @@
+
+
+function Abc(){
+
+    console.log("Hello World");
+}
+
+Abc();
+Abc();
+Abc();
+Abc();
---
+++ script.js
@@ -0,0 +1,31 @@
+let name = prompt("Please enter your name");
+let marks = Number(prompt("Enter your marks"));
+let result;
+
+switch (true) {
+  case marks > 90 && marks <= 100:
+    result = "Great! You are the Topper 🎉";
+    break;
+
+  case marks > 65 && marks <= 90:
+    result = "Congratulations! You got First Class";
+    break;
+
+  case marks > 40 && marks <= 65:
+    result = "You got Second Class";
+    break;
+
+  case marks >= 30 && marks <= 40:
+    result = "You got Third Class";
+    break;
+
+  case marks < 30:
+    result = "Sorry, you are Failed. Better luck next time";
+    break;
+
+  default:
+    result = "Please enter valid marks (0 - 100)";
+}
+
+console.log(name + " : " + result);
+alert(name + " : " + result);
