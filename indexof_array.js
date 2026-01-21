ae7006c2df8446fea120d1b3befd7498a78eec76
manojunicorn <your-github-manojaug07@gmail.com>
2026-01-21 20:13:45 +0530

    Day:5 JavaScript practice

---
+++ indexof_array.js
@@ -0,0 +1,6 @@
+//indexOf() = return the first index at which a given element can be found in an Array, or - 1 if it is not ProgressEvent.
+
+
+let students = ["sanju","abhi","sanjay","atish"];
+
+console.log(students.indexOf("manu"))
