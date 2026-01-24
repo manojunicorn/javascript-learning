b0ab0659d6832826049855192f20cf847652d7a6
manojunicorn <your-github-manojaug07@gmail.com>
2026-01-24 22:56:02 +0530

    Day:7  JavaScript practice

---
+++ pratice.js
@@ -0,0 +1,53 @@
+
+//console.log("hello world");
+
+//let a = 5;
+//let b = 3;
+//console.log(a+b);
+
+//let num = 5;
+//let square = num * num;
+//console.log(square)
+
+//let num = 4;
+
+//if (num % 2 === 0) {
+ // console.log("Even");
+//}else {
+  //console.log("Odd");
+//}
+
+
+//let num = -2;
+
+//if (num === 0) {
+  //console.log("Number is zero");
+//} else if (num > 0) {
+  //console.log("Number is positive");
+//} else {
+  //console.log("Number is negative");
+//}
+
+//let a = 2;
+//let b = 4;
+
+//if (a > b) {
+  //console.log("a is greater");
+//} else {
+ // console.log("b is greater");
+//}
+
+
+let a = 10;
+let b = 20;
+let c = 30;
+
+if (a > b && a > c) {
+  console.log("a is greater");
+} else if (b > a && b > c) {
+  console.log("b is greater");
+} else {
+  console.log("c is greater");
+}
+
+ 
