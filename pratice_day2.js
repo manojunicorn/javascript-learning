29edcebf1ace3a17690fd3956a78d17c1afd938a
manojunicorn <your-github-manojaug07@gmail.com>
2026-01-27 22:07:46 +0530

    Day:7  JavaScript practice

---
+++ pratice_day2.js
@@ -0,0 +1,49 @@
+
+//for (let i = 0; i<=11; i++)
+//console.log(i);
+
+
+//let num = 2;
+//if (num % 2 == 0){
+  //  console.log("even");
+//}else {
+  //  console.log("odd");
+//}
+
+
+//let sum = 0;
+//for (i=0; i>=5; i--);
+//console.log(0);
+
+// let num = -1;
+// if (num == 1){
+//     console.log("number is positive")
+// }
+// else if (num == 0){
+//     console.log("number is zero");
+// }
+// else {
+//     console.log("number is negative"); 
+// }
+
+
+// let sum = 0;
+// for (i=1; i<=5; i++){
+//   sum = sum + i;
+// }
+// console.log(sum);
+
+
+
+// let a = 4;
+// let b = 5;
+// if (a > b){
+//   console.log("a is greater");
+// } 
+// else {
+// console.log("b is greater");
+// }
+
+
+let name = ["manu", "hari", "jagi","mani"];
+console.log(name.length);
