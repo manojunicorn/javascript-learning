8c737b81cc5178b8150afbf680f3aa3460b0bffb
manojunicorn <your-github-manojaug07@gmail.com>
2026-01-30 22:36:59 +0530

    Day:10  JavaScript practice

--- pratice_day3.js
+++ pratice_day3.js
@@ -20,8 +20,79 @@
 
 // greet();
 
-function greet(name){
-    console.log("hello " + name);
-}
-greet("Manoj");
-greet("Sridhar");
+// function greet(name){
+//     console.log("hello " + name);
+// }
+// greet("Manoj");
+// greet("Sridhar");
+
+
+// function add(a,b){
+//     return a+b;
+
+// }
+// let result = add(5,3);
+// console.log(result);
+
+// let res = add(3,3);
+// console.log(res);
+
+// funtion with Return:
+
+// function multiply (x,y,z){
+//    return x*y*z;
+// }
+
+// let res = multiply(2,3,4);
+// console.log(res);
+
+
+// function getFive(num) {
+//   return num;
+// }
+// let num = (5)
+// console.log(num);
+
+
+
+// function givenumber(num){
+//     return(num);
+// }
+// console.log(givenumber(5));
+
+
+
+// function double(n) {
+//   return n * 3;
+// }
+
+// console.log(double(4));
+
+
+// function square(n){
+//  return n * n;
+// }
+// console.log(square(5));
+
+
+// function tripleIfPositive(n) {
+//   if (n > 0) {
+//     return n * 3;
+//   } else {
+//     return n;
+//   }
+// }
+
+// console.log(tripleIfPositive(3));   // 9
+// console.log(tripleIfPositive(-2));  // -2
+
+
+function result(marks){
+  if(marks >= 35){
+    return marks;
+  }else{
+    return fail;
+  }
+  }
+ console.log((marks));
+ 
