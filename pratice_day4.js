4b2954c84a3c8b71f3f87bd6156e0ef96f154bf5
manojunicorn <your-github-manojaug07@gmail.com>
2026-01-31 23:11:18 +0530

    Day:11  JavaScript practice

---
+++ pratice_day4.js
@@ -0,0 +1,78 @@
+// // function sayHello(n){
+// //     return n;
+// // }
+// // let greet = sayHello("hello");
+// // console.log(greet);
+   
+
+
+// function sayHello(){
+//     return "hello";
+// }
+// console.log(sayHello());
+
+// function givenumber(){
+//     return 5;
+// }
+// console.log(givenumber());
+
+
+// function double(num){
+//     return num * 2;
+// }
+// console.log(double(4));
+
+
+// function square(n){
+//     return n*n;
+// }
+// console.log(square(5));
+
+// function result(marks){
+//     if (marks >= 35){
+//         return "pass";
+//     }else{
+//         return "fail";
+//     }
+// }
+// console.log(result(40));
+// console.log(result(20));
+
+// function evenodd(num){
+//     if (num % 2 === 0){
+//         return "even";
+//     }else{
+//         return "odd";
+//     }
+// }
+// console.log(evenodd(2));
+// console.log(evenodd(5));
+
+
+
+// function greatest(a,b){
+//  if (a > b){
+//     return "a is greater";
+//  }else{ 
+//     return "b is greater";
+//  }
+// }
+
+// console.log(greatest(10,20));
+
+// function printNumbers(n) {
+//   for (let i = 1; i <= n; i++) {
+//     console.log(i);
+//   }
+// }
+
+// printNumbers(5);
+
+
+function onetoten(n){
+    for (let i = 1; i <= n; i++) {
+
+    console.log(i)
+    }
+}
+onetoten(11);
