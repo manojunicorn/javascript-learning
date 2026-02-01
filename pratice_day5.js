86ab26666da194d30f76b10a5b4b27e58171def1
manojunicorn <your-github-manojaug07@gmail.com>
2026-02-01 23:59:13 +0530

    Day:12  JavaScript practice

---
+++ pratice_day5.js
@@ -0,0 +1,37 @@
+function sumN(n) {
+  let sum = 0;
+
+  for (let i = 1; i <= n; i++) {
+    sum = sum + i;
+  }
+
+  return sum;
+}
+ console.log(sumN(5)); // 15
+// i = 1, sum = 1, 
+// i = 2, sum = 3,
+// i = 3, sum = 6,
+// i = 4, sum = 10,
+// i = 5, sum = 15,
+// i = 6,
+// C B I
+
+ 
+
+// function evennumber (n){
+//     if (n%2==0){
+//         return "even";
+//     }else if(n%2==1){
+//         return "odd";
+//     }else{
+//         return null;
+//     }
+    
+// }  
+// console.log("1 " + evennumber(4));
+// console.log("2 " + evennumber(5));
+// console.log("3 " + evennumber(88478393));
+
+    
+    
+
