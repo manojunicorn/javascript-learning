3bea1ef4894fd50ece436c6cb2f55feafc757dc6
manojunicorn <your-github-manojaug07@gmail.com>
2026-02-02 22:57:10 +0530

    Day:13  JavaScript practice

---
+++ pratice_day6.js
@@ -0,0 +1,40 @@
+
+
+// input 5
+// output divisible or not
+// loop is not requried
+// condition n % 5 === 0
+
+// function divisible (n){
+//     if ( n % 5 === 0){
+//         return "yes divisible";
+//     }else{
+//         return "not divisible";
+//     }
+
+// }
+// console.log("Give value is : " + divisible(35));
+// console.log(divisible(83393738));
+// console.log(divisible(38373839));
+
+// input 100
+// output given number is greater or not
+// loop is not requried
+// condition n >= 100
+
+// function greater(n) {
+//     if (n >= 100) {
+//         return "Yes given number is greater than or equal to 100";
+//     } else {
+//         return "No Given number is less than 100";
+//     }
+// }
+
+// console.log("Given value is: " + greater(3893));
+// console.log("Given value is: " + greater(65));
+
+for (let i = 5; i >= 1; i--) {
+    
+  console.log(i);
+}
+
